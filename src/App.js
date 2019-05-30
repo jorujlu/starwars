a96078ecbf2bd30693f3a16e00aa242aac84cff2
Jeyhun Orujlu <Jeyhun.Orujlu@ibm.com>
2019-05-30 00:27:44 -0400

    created project structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <h1>Hello, StarWars</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
